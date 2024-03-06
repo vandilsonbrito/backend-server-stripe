53c8709a5d8c15c7ff715263e7ca16f0672343e7
vandilsonbrito <vandilson25@outlook.com>
2024-03-06 09:52:42 -0300

    Feat: Adds middleware cors function

--- index.js
+++ index.js
@@ -6,46 +6,39 @@ import router from './stripe.js';
 const stripe = router;
 
 const app = express();
-app.use(cors({
-  origin: ['https://delicias-da-casa.netlify.app', 'http://localhost:3001'] // Adicione aqui os domínios permitidos
-}));
-const port = process.env.PORT || 3001; 
 
-// Middleware to parse JSON and set up
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://delicias-da-casa.netlify.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// Middleware to enable CORS
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
+// Apply CORS middleware to all routes
+app.use(allowCors);
 
-app.use("/CartCheckout/stripe", stripe)
+// Additional middleware
 app.use(json());
 
+// Routes
+app.use("/CartCheckout/stripe", stripe);
+
+// Default route
 app.get('/', (req, res) => {
   res.send('Servidor do backend está funcionando!');
 });
 
-app.post('/create-checkout-session', async (req, res) => {
-  const { amount, currency } = req.body;
-
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency,
-    });
-
-    res.status(200).json({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    console.error('Erro ao criar o pagamento:', error);
-    res.status(500).send({ error: 'Erro ao criar o pagamento' });
-  }
-});
-
+// Start the server
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Servidor do backend está rodando em http://localhost:${port}`);
 });
-
