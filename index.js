66460583464923f29799c46cf8c1b8a96f59f0ec
vandilsonbrito <vandilson25@outlook.com>
2024-03-06 13:29:50 -0300

    Feat: Update Allow-Origin values

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ const app = express();
 app.use(cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://delicias-da-casa.netlify.app');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-const port =  443; 
+const port =  3000; 
 
 // Middleware to parse JSON and set up
 app.use(express.json()); 
