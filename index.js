2c1d90479c7dd70ab849587f6386589695f23239
vandilsonbrito <vandilson25@outlook.com>
2024-03-06 15:15:07 -0300

    Feat: Change source

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import router from './stripe.js';
 const stripe = router;
 
 const app = express();
-/* app.use(cors()); */
-/* app.use((req, res, next) => {
+app.use(cors());
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization');
@@ -16,22 +16,7 @@ const app = express();
   } else {
     next();
   }
-}); */
-
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-}
-
-app.use("/CartCheckout/stripe", allowCors(stripe));
-
+});
 
 
 
@@ -41,7 +26,7 @@ const port =  3000;
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 
-/* app.use("/CartCheckout/stripe", stripe) */
+app.use("/CartCheckout/stripe", stripe)
 app.use(json());
 
 app.get('/', (req, res) => {
