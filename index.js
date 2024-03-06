c97e4905b378fce3613a0d9f54b5131e80dea601
vandilsonbrito <vandilson25@outlook.com>
2024-03-06 16:12:49 -0300

    Feat: Try writeHead(200)

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization');
   if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
+    res.writeHead(200);
   } else {
     next();
   }
