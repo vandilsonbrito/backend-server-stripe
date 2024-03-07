b18376763f97d6cd3cbc5b3404b2b7c3b30bafb0
vandilsonbrito <vandilson25@outlook.com>
2024-03-07 10:54:07 -0300

    Feat: delete micro-cors

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* import express from 'express';
+import express from 'express';
 import { json } from 'express';
 
 
@@ -40,54 +40,3 @@ app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
 
- */
-
-import express from 'express';
-import micro from "micro-cors";
-import router from './stripe.js';
-
-const stripe = router;
-const app = express();
-const port = 3000;
-
-// Middleware to parse JSON and set up
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Defina a função do seu aplicativo
-function MyApi(req, res) {
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-  // handling other requests normally after this
-}
-
-// Inicialize o micro-cors
-const cors = micro();
-
-// Use o micro-cors como middleware para todas as rotas
-app.use(cors(MyApi));
-
-// Defina suas rotas
-app.use("/CartCheckout/stripe", stripe);
-
-app.get('/', (req, res) => {
-  res.send('Server is running!');
-});
-
-app.post('/create-checkout-session', async (req, res) => {
-  const { amount, currency } = req.body;
-
-  try {
-    // Código para criar um pagamento aqui
-    res.status(200).json({ message: 'Payment created successfully' });
-  } catch (error) {
-    console.error('Error creating Payment: ', error);
-    res.status(500).send({ error: 'Error creating Payment' });
-  }
-});
-
-// Inicie o servidor
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
