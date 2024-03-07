4c938494b7836a92d3dc0d45430212fa632422ef
vandilsonbrito <vandilson25@outlook.com>
2024-03-07 10:41:54 -0300

    Adds micro-cors

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-import express from 'express';
+/* import express from 'express';
 import { json } from 'express';
-/* import cors from 'cors'; */
+
 
 import router from './stripe.js';
 const stripe = router;
 
 const app = express();
-/* app.use(cors()); */
-/* app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization');
-  if (req.method === 'OPTIONS') {
-    console.log('Status code before setting to 200:', res.statusCode);
-    res.status(200).send(http.StatusOK);
-  } else {
-    next();
-  }
-  
-}); */
 
 const port =  3000; 
 
@@ -53,3 +40,54 @@ app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
 
+ */
+
+import express from 'express';
+import microCors from 'micro-cors';
+import router from './stripe.js';
+
+const stripe = router;
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON and set up
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Defina a função do seu aplicativo
+function MyApi(req, res) {
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  // handling other requests normally after this
+}
+
+// Inicialize o micro-cors
+const cors = microCors();
+
+// Use o micro-cors como middleware para todas as rotas
+app.use(cors);
+
+// Defina suas rotas
+app.use("/CartCheckout/stripe", stripe);
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+app.post('/create-checkout-session', async (req, res) => {
+  const { amount, currency } = req.body;
+
+  try {
+    // Código para criar um pagamento aqui
+    res.status(200).json({ message: 'Payment created successfully' });
+  } catch (error) {
+    console.error('Error creating Payment: ', error);
+    res.status(500).send({ error: 'Error creating Payment' });
+  }
+});
+
+// Inicie o servidor
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
